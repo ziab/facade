e5f44dda3daaa48bd75a0d2acfa379110270ae64
Danil Viazovoy <dviazovoy@nvidia.com>
2019-05-03 20:56:52 -0700

    initial commit

---
+++ facade/facade.cpp
@@ -0,0 +1,34 @@
+﻿#include "facade.h"
+
+#include <iostream>
+#include <string>
+
+class foo
+{
+public:
+    foo() {};
+    int do_stuff(bool param1, int param2, const std::string& param3)
+    {
+        if (param1 == true && param2 == 42) return 1;
+        return 0;
+    }
+};
+
+class foo_facade : public facade::facade<foo>
+{
+public:
+    FACADE_CONSTRUCTOR(foo_facade);
+    FACADE_METHOD(do_stuff);
+};
+
+int main()
+{
+	std::cout << "Hello CMake." << std::endl;
+
+    auto impl = std::make_unique<foo>();
+    foo_facade foo{ std::move(impl) };
+
+    foo.do_stuff(false, 3, "hello!");
+    foo.do_stuff(true, 42, "hello again!");
+	return 0;
+}
---
+++ facade/facade.h
@@ -0,0 +1,50 @@
+﻿#pragma once
+#include <memory>
+#include <any>
+#include <unordered_map>
+#include <chrono>
+
+#define FACADE_METHOD(_name) \
+template<typename ...t_args>\
+auto _name(t_args&& ... args)\
+{\
+    return call_method(\
+        m_impl,\
+        &t_impl_type::_name,\
+        std::forward<t_args>(args)...);\
+}\
+
+#define FACADE_CONSTRUCTOR(_name) \
+_name(t_impl_type& impl) : facade(impl) {}\
+_name(std::unique_ptr<t_impl_type> ptr) : facade(std::move(ptr)) {}\
+
+namespace facade
+{
+    struct method_call
+    {
+        std::vector<std::any> args;
+        std::any ret;
+        std::chrono::time_point<std::chrono::system_clock> timestamp;
+    };
+
+    template<typename t_type>
+    class facade
+    {
+    protected:
+        std::unique_ptr<t_type> m_ptr;
+        t_type& m_impl;
+        std::unordered_map<std::string, std::vector<method_call>> m_calls;
+
+        template <typename t_obj, typename t_ret, class ...t_expected_args, typename ...t_actual_args>
+        t_ret call_method(t_obj& obj, t_ret(t_obj::* method)(t_expected_args...), t_actual_args&& ... args)
+        {
+            return (obj.*method)(std::forward<t_actual_args>(args)...);
+        }
+
+    public:
+        using t_impl_type = t_type;
+
+        facade(t_type& impl) : m_impl(impl) {}
+        facade(std::unique_ptr<t_type>&& ptr) : m_ptr(std::move(ptr)), m_impl(*m_ptr) {}
+    };
+}
